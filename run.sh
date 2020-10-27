d5662ec967d786f9dd42e9bab1b6c7fda01cbb44
Philipp Kutin <philipp.kutin@gmail.com>
2020-10-27 19:08:57 +0100

    Make ~/.Xauthority read-only in the container, fix mount of ~/Documents.

--- run.sh
+++ run.sh
@@ -17,6 +17,8 @@ fi
 # TODO: add an argument once we support more than one version.
 DEFAULT_RCU_VERSION=r2020-003
 
+guestHome="/home/$USER"
+
 # TODO: make this configurable somehow?
 MountArgs=( \
   -v "$HOME/Documents:$guestHome/Documents:ro" \
@@ -32,14 +34,12 @@ fi
 
 ## ----------
 
-guestHome="/home/$USER"
-
 docker run -it --rm \
        --net=host \
        --hostname=rM-rcu \
        --add-host='remarkable:10.11.99.1' \
        -e DISPLAY \
-       -v "$HOME/.Xauthority:$guestHome/.Xauthority" \
+       -v "$HOME/.Xauthority:$guestHome/.Xauthority:ro" \
        -v "$HOME/.local/share/davisr/rcu:$guestHome/.local/share/davisr/rcu" \
        "${MountArgs[@]}" \
        "${EntryPointArgs[@]}" \
